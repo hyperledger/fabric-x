40b3186eac75092d63bb853034e6a24b177fd028
Anushka <anush159@in.ibm.com>
2025-08-19 22:32:47 +0530

    Imported only the cmd from fabric-x-common
    Signed-off-by: Anushka <anush159@in.ibm.com>

---
+++ Makefile
@@ -0,0 +1,82 @@
+# Copyright IBM Corp All Rights Reserved.
+# Copyright London Stock Exchange Group All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# -------------------------------------------------------------
+# Run `make help` to find the supported targets
+
+# Disable implicit rules
+.SUFFIXES:
+MAKEFLAGS += --no-builtin-rules
+
+BUILD_DIR ?= bin
+
+PKGNAME = github.com/hyperledger/fabric-x-common
+PKGNAME2 = github.com/hyperledger/fabric-x/tools
+
+GO_TAGS ?=
+
+go_cmd          ?= go
+go_test         ?= $(go_cmd) test -json -v -timeout 30m
+
+TOOLS_EXES = configtxgen configtxlator cryptogen
+
+pkgmap.configtxgen    := $(PKGNAME2)/configtxgen
+pkgmap.configtxlator  := $(PKGNAME2)/configtxlator
+pkgmap.cryptogen      := $(PKGNAME2)/cryptogen
+
+.DEFAULT_GOAL := help
+
+MAKEFLAGS += --jobs=16
+
+.PHONY: help
+# List all commands with documentation
+help: ## List all commands with documentation
+	@echo "Available commands:"
+	@awk 'BEGIN {FS = ":.*?## "}; /^[a-zA-Z_-]+:.*?## / {printf "\033[36m%-15s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
+
+.PHONY: tools
+tools: $(TOOLS_EXES) ## Builds all tools
+
+GO_TEST_FMT_FLAGS := -hide empty-packages
+
+## Run all tests
+test: FORCE
+	@$(go_test) ./... | gotestfmt ${GO_TEST_FMT_FLAGS}
+
+.PHONY: $(TOOLS_EXES)
+$(TOOLS_EXES): %: $(BUILD_DIR)/% ## Builds a native binary
+
+$(BUILD_DIR)/%: GO_LDFLAGS = $(METADATA_VAR:%=-X $(PKGNAME)/common/metadata.%)
+$(BUILD_DIR)/%:
+	@echo "Building $@"
+	@mkdir -p $(@D)
+	@GOBIN=$(abspath $(@D)) go install -tags "$(GO_TAGS)" -ldflags "$(GO_LDFLAGS)" -buildvcs=false $(pkgmap.$(@F))
+	@touch $@
+
+.PHONY: clean
+clean: ## Cleans the build area
+	-@rm -rf $(BUILD_DIR)
+
+lint: FORCE
+	@echo "Running Go Linters..."
+	golangci-lint run --color=always --new-from-rev=main --timeout=4m
+	@echo "Running License Header Linters..."
+	scripts/license-lint.sh
+
+# Build the fabric-x-tools image for the current machine platform.
+.PHONY: build-fabric-x-tools-image
+build-fabric-x-tools-image:
+	./tools/images/build_image.sh --tag docker.io/hyperledger/fabric-x-tools:$(IMAGE_TAG) -f ./tools/images/Dockerfile
+
+# Build the fabric-x-tools image for multiple platforms.
+.PHONY: build-fabric-x-tools-multiplatform-image
+build-fabric-x-tools-multiplatform-image:
+	./tools/images/build_image.sh --tag docker.io/hyperledger/fabric-x-tools:$(IMAGE_TAG) -f ./tools/images/Dockerfile --multiplatform --push
+
+# https://www.gnu.org/software/make/manual/html_node/Force-Targets.html
+# If a rule has no prerequisites or recipe, and the target of the rule is a nonexistent file,
+# then make imagines this target to have been updated whenever its rule is run.
+# This implies that all targets depending on this one will always have their recipe run.
+FORCE:
---
+++ tools/configtxgen/main.go
@@ -0,0 +1,142 @@
+/*
+Copyright IBM Corp. 2017 All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/hyperledger/fabric-lib-go/bccsp/factory"
+
+	"github.com/hyperledger/fabric-x-common/common/util"
+	"github.com/hyperledger/fabric-x-common/internaltools/configtxgen"
+	"github.com/hyperledger/fabric-x-common/internaltools/configtxgen/genesisconfig"
+	"github.com/hyperledger/fabric-x-common/internaltools/configtxgen/metadata"
+)
+
+var logger = util.MustGetLogger("common.tools.configtxgen")
+
+func main() {
+	var outputBlock, outputChannelCreateTx, channelCreateTxBaseProfile, profile, configPath, channelID, inspectBlock, inspectChannelCreateTx, asOrg, printOrg string
+
+	flag.StringVar(&outputBlock, "outputBlock", "", "The path to write the genesis block to (if set)")
+	flag.StringVar(&channelID, "channelID", "", "The channel ID to use in the configtx")
+	flag.StringVar(&outputChannelCreateTx, "outputCreateChannelTx", "", "[DEPRECATED] The path to write a channel creation configtx to (if set)")
+	flag.StringVar(&channelCreateTxBaseProfile, "channelCreateTxBaseProfile", "", "[DEPRECATED] Specifies a profile to consider as the orderer system channel current state to allow modification of non-application parameters during channel create tx generation. Only valid in conjunction with 'outputCreateChannelTx'.")
+	flag.StringVar(&profile, "profile", "", "The profile from configtx.yaml to use for generation.")
+	flag.StringVar(&configPath, "configPath", "", "The path containing the configuration to use (if set)")
+	flag.StringVar(&inspectBlock, "inspectBlock", "", "Prints the configuration contained in the block at the specified path")
+	flag.StringVar(&inspectChannelCreateTx, "inspectChannelCreateTx", "", "[DEPRECATED] Prints the configuration contained in the transaction at the specified path")
+	flag.StringVar(&asOrg, "asOrg", "", "Performs the config generation as a particular organization (by name), only including values in the write set that org (likely) has privilege to set")
+	flag.StringVar(&printOrg, "printOrg", "", "Prints the definition of an organization as JSON. (useful for adding an org to a channel manually)")
+
+	version := flag.Bool("version", false, "Show version information")
+
+	flag.Parse()
+
+	if channelID == "" && (outputBlock != "" || outputChannelCreateTx != "") {
+		logger.Fatalf("Missing channelID, please specify it with '-channelID'")
+	}
+
+	// show version
+	if *version {
+		printVersion()
+		os.Exit(0)
+	}
+
+	// don't need to panic when running via command line
+	defer func() {
+		if err := recover(); err != nil {
+			if strings.Contains(fmt.Sprint(err), "Error reading configuration: Unsupported Config Type") {
+				logger.Error("Could not find configtx.yaml. " +
+					"Please make sure that FABRIC_CFG_PATH or -configPath is set to a path " +
+					"which contains configtx.yaml")
+				os.Exit(1)
+			}
+			if strings.Contains(fmt.Sprint(err), "Could not find profile") {
+				logger.Error(fmt.Sprint(err) + ". " +
+					"Please make sure that FABRIC_CFG_PATH or -configPath is set to a path " +
+					"which contains configtx.yaml with the specified profile")
+				os.Exit(1)
+			}
+			logger.Panic(err)
+		}
+	}()
+
+	logger.Info("Loading configuration")
+	err := factory.InitFactories(nil)
+	if err != nil {
+		logger.Fatalf("Error on initFactories: %s", err)
+	}
+	var profileConfig *genesisconfig.Profile
+	if outputBlock != "" || outputChannelCreateTx != "" {
+		if profile == "" {
+			logger.Fatalf("The '-profile' is required when '-outputBlock', '-outputChannelCreateTx' is specified")
+		}
+
+		if configPath != "" {
+			profileConfig = genesisconfig.Load(profile, configPath)
+		} else {
+			profileConfig = genesisconfig.Load(profile)
+		}
+	}
+
+	var baseProfile *genesisconfig.Profile
+	if channelCreateTxBaseProfile != "" {
+		if outputChannelCreateTx == "" {
+			logger.Warning("Specified 'channelCreateTxBaseProfile', but did not specify 'outputChannelCreateTx', 'channelCreateTxBaseProfile' will not affect output.")
+		}
+		if configPath != "" {
+			baseProfile = genesisconfig.Load(channelCreateTxBaseProfile, configPath)
+		} else {
+			baseProfile = genesisconfig.Load(channelCreateTxBaseProfile)
+		}
+	}
+
+	if outputBlock != "" {
+		if err := configtxgen.DoOutputBlock(profileConfig, channelID, outputBlock); err != nil {
+			logger.Fatalf("Error on outputBlock: %s", err)
+		}
+	}
+
+	if outputChannelCreateTx != "" {
+		if err := configtxgen.DoOutputChannelCreateTx(profileConfig, baseProfile, channelID, outputChannelCreateTx); err != nil {
+			logger.Fatalf("Error on outputChannelCreateTx: %s", err)
+		}
+	}
+
+	if inspectBlock != "" {
+		if err := configtxgen.DoInspectBlock(inspectBlock); err != nil {
+			logger.Fatalf("Error on inspectBlock: %s", err)
+		}
+	}
+
+	if inspectChannelCreateTx != "" {
+		if err := configtxgen.DoInspectChannelCreateTx(inspectChannelCreateTx); err != nil {
+			logger.Fatalf("Error on inspectChannelCreateTx: %s", err)
+		}
+	}
+
+	if printOrg != "" {
+		var topLevelConfig *genesisconfig.TopLevel
+		if configPath != "" {
+			topLevelConfig = genesisconfig.LoadTopLevel(configPath)
+		} else {
+			topLevelConfig = genesisconfig.LoadTopLevel()
+		}
+
+		if err := configtxgen.DoPrintOrg(topLevelConfig, printOrg); err != nil {
+			logger.Fatalf("Error on printOrg: %s", err)
+		}
+	}
+}
+
+func printVersion() {
+	fmt.Println(metadata.GetVersionInfo())
+}
