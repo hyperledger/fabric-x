36efb467cdba4e11e4bdf1cc61ce0622bed2a51a
pco <pasquale.convertini@ibm.com>
2026-02-05 10:14:04 +0100

    fixed bug: added `init_fabricx` back
    Signed-off-by: pco <pasquale.convertini@ibm.com>

--- samples/tokens/scripts/test.sh
+++ samples/tokens/scripts/test.sh
@@ -6,36 +6,50 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Function to print a section title
+CONTAINER_CLI="${CONTAINER_CLI:-docker}"
+
+## Print a section title
 function print_section_header() {
     echo "# ========================="
-    echo "$1"
+    echo "# $1"
     echo "# ========================="
 }
 
-# Function to handle cleanup when the script is aborted
+## Cleanup and stop network on abort
 function cleanup() {
-    # stop any spinner message
-    print_section_header "Stopping tests..."
-    # remove lock if we own it
-    make teardown clean
+    stop_network
     exit 1
 }
 
-# init FabricX
-function init_fabricx() {
+## Setup and start the network
+function run_network() {
+    print_section_header "Setup and start the network..."
+    make setup start
+    
     if [[ "$PLATFORM" == "fabricx" ]]; then
-        print_section_header "Initializing ${PLATFORM}..."
-        curl -X POST http://localhost:9300/endorser/init
+        init_fabricx
     fi
 }
 
-# Function to run tests to verify whether the network works as expected
+## Stop and clean up the network
+function stop_network() {
+    print_section_header "Stopping network..."
+    make teardown clean
+}
+
+## Initialize FabricX if needed
+function init_fabricx() {
+    print_section_header "Initializing ${PLATFORM}..."
+    curl -X POST http://localhost:9300/endorser/init
+}
+
+## Run tests to verify the network
 function run_test() {
     # test application
     print_section_header "Run tests"
 
-    # currently we wait manually with a sleep. TODO: add an healthcheck within the `docker-compose`
+    # currently we wait manually with a sleep.
+    # TODO: add an healthcheck within the `docker-compose`
     sleep 10
     curl http://localhost:9100/issuer/issue -d '{
         "amount": {"code": "TOK","value": 1000},
@@ -53,13 +67,9 @@ function run_test() {
     curl -X GET http://localhost:9500/owner/accounts/alice/transactions | jq
 }
 
-# prepare script
-# trap signals to ensure cleanup and lock release
-# note: INT/TERM go to cleanup (which exits).
+# Script Start
 trap cleanup INT TERM
 
-print_section_header "Setup and start the network..."
-make setup start
-
+run_network
 run_test
-make teardown clean
+stop_network
